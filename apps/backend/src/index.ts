2d662b46a6c287fee3087fc794ba935325e7e9d4
Paulo de Oliveira Gontijo <Paulo10.oliveira.gontijo@gmail.com>
2026-03-10 17:07:00 -0300

    Update of files in frontend and backend

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -5,8 +5,19 @@ import { authRoutes } from "./routes/auth.routes";
 
 const door = process.env.PORT || 3000;
 
+// Puxa a URL do frontend das variáveis de ambiente
+// Se não existir (desenvolvimento local), libera o localhost padrão do Vite
+const frontEndUrl = process.env.FRONTEND_URL || 'http://localhost:5173';
+
 const app = new Elysia()
-    .use(cors())
+    .use(
+        cors({
+      origin: frontEndUrl, // A Lista VIP: Apenas este endereço pode fazer requisições
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // O que eles podem fazer lá dentro
+      allowedHeaders: ['Content-Type', 'Authorization'], // Quais dados extras podem trazer
+      credentials: true, // Necessário se você for usar Cookies ou Sessões no futuro
+     })
+    )
     .use(swagger())
     .get('/', () => 'API Empreenderjuntos - Online')
     // Registra rotas de autenticação 
--- apps/frontend/src/services/api.ts
+++ apps/frontend/src/services/api.ts
@@ -1,8 +1,10 @@
 import { RegisterFormData } from "../schemas/registerSchema";//importação de Tipo (Interface) gerado automaticamente pelo Zod (z.infer). Isso garante que, se você mudar o esquema do sócio/fundador, o TypeScript avisará se esta função estiver enviando dados incompletos
 
+const API_URL = import.meta.env.VITE_API_URL;
+
 //função de chamada para a API
 export async function registerCompany(data: RegisterFormData){
-    const response = await fetch("http://localhost:3000/api/v1/users/register", {
+    const response = await fetch(`${API_URL}/api/v1/users/register`, {
         method: "POST",
         headers:{
             "Content-Type": "application/json",
